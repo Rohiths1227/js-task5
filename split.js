f9a779304e5be0092f1810d5dd436a277232b2fe
Rohiths1227 <rohithofficial1227@gmail.com>
2026-03-12 12:59:02 +0530

    Add files via upload

---
+++ split.js
@@ -0,0 +1,10 @@
+let url = "www-stackly-com";
+
+
+// 1️⃣ Convert the string into an array
+// split("-")
+
+let urlArray = url.split("-");
+
+console.log("URL converted to Array:");
+console.log(urlArray);
