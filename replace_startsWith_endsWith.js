3b5c48dce4529121e5a3b11e071ce2191190599a
Rohiths1227 <rohithofficial1227@gmail.com>
2026-03-12 13:02:16 +0530

    Add files via upload

---
+++ replace_startsWith_endsWith.js
@@ -0,0 +1,28 @@
+let role = "python developer";
+
+
+//  Replace "python" with "javascript"
+// replace()
+
+let newRole = role.replace("python","full stack");
+
+console.log("Updated Role:");
+console.log(newRole);
+
+
+//  Check if role starts with "python"
+// startsWith()
+
+let startsCheck = role.startsWith("python");
+
+console.log("Does role start with 'python'?");
+console.log(startsCheck);
+
+
+// Check if role ends with "developer"
+// endsWith()
+
+let endsCheck = role.endsWith("developer");
+
+console.log("Does role end with 'developer'?");
+console.log(endsCheck);
